e7fdb9f02001694af0b1d2e1bfd70e69b7522908
Biozard01 <arthur.laforest33@gmail.com>
2021-04-01 13:04:44 +0200

    avancé du projet

--- BridgeMonitor/Controllers/HomeController.cs
+++ BridgeMonitor/Controllers/HomeController.cs
@@ -2,36 +2,43 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using BridgeMonitor.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace BridgeMonitor.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult TTLesFerme()
         {
-            return View();
+            var info = GetFromAPI();
+            return View(info);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private static List<TTFermetures> GetFromAPI()
+        {
+            using var client = new HttpClient();
+
+            var response = client.GetAsync("https://api.alexandredubois.com/pont-chaban/api.php");
+            var stringResult = response.Result.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<List<TTFermetures>>(stringResult.Result);
+            return result;
         }
     }
 }
