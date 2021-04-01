304a2f5097cfd26c4a1c262c41dbff41450acdc4
Biozard01 <arthur.laforest33@gmail.com>
2021-04-01 10:41:17 +0200

    clean start

---
+++ BridgeMonitor/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace BridgeMonitor.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
