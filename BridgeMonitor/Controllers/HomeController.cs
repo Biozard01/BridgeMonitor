52997ed40abc76eb6d236015d2b7b222113e5b7d
Biozard01 <arthur.laforest33@gmail.com>
2021-04-02 20:22:30 +0200

    prochaine fermeture cleaner

--- BridgeMonitor/Controllers/HomeController.cs
+++ BridgeMonitor/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using BridgeMonitor.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -13,32 +15,71 @@ namespace BridgeMonitor.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
         public IActionResult Index()
         {
+            List<TTFermetures> info = GetFromAPI();
+            info.Sort((s1, s2) => DateTimeOffset.Compare(s1.ClosingDate, s2.ClosingDate));
+
+            foreach (var ferme in info)
+            {
+                if (DateTimeOffset.Compare(DateTimeOffset.Now, ferme.ClosingDate) < 0)
+                {
+                    ViewData["Info"] = ferme;
+                    break;
+                }
+            }
+
             return View();
+        }
+
+        public IActionResult Detail(string fermes)
+        {
+            string[] infos = fermes.Split(".");
+            List<TTFermetures> info = GetFromAPI();
+            List<TTFermetures> ancienInfo = new List<TTFermetures>();
+            for (int i = 0; i < info.Count; i++)
+            {
+                if (info[i].ClosingDate.CompareTo(DateTime.Now) < 0)
+                {
+                    ancienInfo.Add(info[i]);
+                    info.RemoveAt(i);
+                }
+            }
+            
+            info.Sort((s1, s2) => DateTimeOffset.Compare(s1.ClosingDate, s2.ClosingDate));
+            ancienInfo.Sort((s1, s2) => DateTimeOffset.Compare(s1.ClosingDate, s2.ClosingDate));
+
+            ViewData["Info"] = infos[0] == "n" ? info[int.Parse(infos[1])] : ancienInfo[int.Parse(infos[1])];
+            return View("Index");
         }
 
         public IActionResult TTLesFerme()
         {
-            var info = GetFromAPI();
-            return View(info);
+            ViewData["Infos"] = GetFromAPI();
+            return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        private static List<TTFermetures> GetFromAPI()
+        public static List<TTFermetures> GetFromAPI()
         {
-            using var client = new HttpClient();
-
-            var response = client.GetAsync("https://api.alexandredubois.com/pont-chaban/api.php");
-            var stringResult = response.Result.Content.ReadAsStringAsync();
-            var result = JsonConvert.DeserializeObject<List<TTFermetures>>(stringResult.Result);
-            return result;
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync("https://api.alexandredubois.com/pont-chaban/api.php");
+                var stringResult = response.Result.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<List<TTFermetures>>(stringResult.Result);
+                return result;
+            }
         }
     }
 }
